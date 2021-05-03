2528a8babc2481421e7d99c913ad073918efa437
avetter1998 <54647171+avetter1998@users.noreply.github.com>
2021-05-03 13:31:05 -0400

    added GA

---
+++ runGenetic.py
@@ -0,0 +1,11 @@
+import geneticAlgo
+
+# 1st stringLength (do not change)
+# 2nd population size (can change if needed, probably not likely)
+# 3rd number of generations (can change, leave low to test performance first. Likely will need to be higher for final run
+# 4th probability of mutation (can change but likely not needed)
+# 5th probability of crossover (can change slightly, might be worth experimenting with)
+ga = geneticAlgo.sga(4, 10, 5, .001, .4)
+
+ga.runGA()
+

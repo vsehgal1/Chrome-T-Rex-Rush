f25ea75f82930f7b3a1420a913f8c65397f6254f
vsehgal <vikramsehgal@live.in>
2021-05-03 13:10:48 -0400

    updates

--- genetic.py
+++ genetic.py
@@ -1,14 +1,12 @@
 import main
 
-max_chrome = (0,'')
-
-chromes = ['1010', '1000']
-
 def run_game(chrome_val):
     get_high = (main.main(chrome_val))
     print('Current Max: '+ str(get_high))
     print('Current Ruleset: '+ str(chrome_val))
     return get_high
+
+run_game('1000')
 
 # gonna be return type
 # print(max_chrome)
